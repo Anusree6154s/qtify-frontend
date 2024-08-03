f87497bce2beb6016a2371384dd80356f012120b
Anusree <anilkumaranusree113@gmail.com>
2024-08-03 19:03:31 +0530

    build qtify

---
+++ qtify/src/components/Button/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+export default function ButtonGroup({ children }) {
+    return <Button variant="outlined">{children}</Button>
+}
